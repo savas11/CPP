d971ccb8f49a2b5ed7adc722b940b11325f819d0
Özgür Savaş <51775407+savas11@users.noreply.github.com>
2023-08-11 07:42:11 +0000

    Brueaucrat start

--- cpp05/ex00/Bureaucrat.hpp
+++ cpp05/ex00/Bureaucrat.hpp
@@ -1,20 +1,35 @@
 #pragma once
 
-#include <iostream>
 #include <string>
+#include <iostream>
 
-
-class Bureucrat {
-private:
-    std::string const _name ;
-    int _gradePoints;
+class Bureaucrat {
+protected:
+	const std::string name;
+	int grade;
 public:
-   int GradeTooHighException(150);
-   int  GradeTooLowException(1);
+	Bureaucrat(const std::string _name, int _grade);
+	Bureaucrat(const Bureaucrat& _copy);
+	const Bureaucrat& operator=(const Bureaucrat& _copy);
+	~Bureaucrat();
 
-   void const getName(void);
-   std::string setName(std::string name);
+	const	std::string   getName(void) const;
+	void	setName(std::string _n) ;
+	int		getGrade(void) const;
+	void	setGrade(int _grade);
 
-    void const getGrade(void);
+	void	increment(void); 
+	void	decrement(void);
+
+	class GradeTooLowException : public std::exception {
+		public:
+			virtual const char *what() const throw();
+	};
+
+	class GradeTooHighException : public std::exception {
+		public:
+			virtual const char *what() const throw();
+	};
 };
 
+std::ostream&   operator<<(std::ostream& o, const Bureaucrat& b);
